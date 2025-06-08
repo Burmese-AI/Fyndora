55d15b4a1315729b3330ac957d1c498cca099d10
TharHtet236401 <tharhteta3@gmail.com>
2025-06-08 17:27:57 +0700

    Add login_required decorator to profileTest view for access control; update signup.html and nav_bar.html for improved styling and user authentication display.

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required   
 
 
 # Create your views here.
+@login_required
 def profileTest(request):
     return render(request, "accounts/profile_test.html")

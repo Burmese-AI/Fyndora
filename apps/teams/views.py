abea521916613502b94f178415f8df7e61ea1f9c
TharHtet236401 <tharhteta3@gmail.com>
2025-06-10 19:19:56 +0700

    add Login required to workspaceListView for security

--- apps/teams/views.py
+++ apps/teams/views.py
@@ -1,9 +1,10 @@
 from django.views.generic import ListView
 from apps.teams.selectors import get_all_team_members
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
-class TeamListView(ListView):
+class TeamListView(ListView, LoginRequiredMixin):
     template_name = "teams/team_list.html"
     context_object_name = "teams"
 

f5fc9c1dcf27c75b756ad9d99c7e0659bda97246
TharHtet236401 <tharhteta3@gmail.com>
2025-06-12 16:36:46 +0700

    fixing the error of django htmx module

--- apps/core/selectors.py
+++ apps/core/selectors.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 def get_user_by_email(email: str) -> Optional[User]:
     """Get user by email"""
     return User.objects.filter(email=email).first()

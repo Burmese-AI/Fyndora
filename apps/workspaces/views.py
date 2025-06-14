96f2c17c598ee989bba62053cf8f3501a703bc69
TharHtet236401 <tharhteta3@gmail.com>
2025-06-14 15:12:15 +0700

    Add error handling in edit_workspace function; implement try-catch for workspace retrieval and form submission

--- apps/workspaces/views.py
+++ apps/workspaces/views.py
@@ -12,8 +12,9 @@ from apps.workspaces.selectors import (
 )
 from apps.workspaces.services import create_workspace_from_form
 from django.contrib import messages
-from apps.workspaces.exceptions import WorkspaceCreationError
+from apps.workspaces.exceptions import WorkspaceCreationError, WorkspaceUpdateError
 from apps.workspaces.selectors import get_workspace_by_id, get_orgMember_by_user_id_and_organization_id
+from apps.workspaces.services import update_workspace_from_form
 
 
 # Create your views here.
@@ -67,20 +68,33 @@ def create_workspace(request, organization_id):
 
 
 def edit_workspace(request, organization_id, workspace_id):
-    workspace = get_workspace_by_id(workspace_id)
-    organization = get_organization_by_id(organization_id)
-    print(workspace)
-    print(organization)
-    if request.method == "POST":
-        form = WorkspaceForm(request.POST, instance=workspace)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Workspace updated successfully.")
+    try:
+        workspace = get_workspace_by_id(workspace_id)
+        organization = get_organization_by_id(organization_id)
+        
+        if not workspace or not organization:
+            messages.error(request, "Workspace or organization not found.")
             return HttpResponseClientRedirect(f"/{organization_id}/workspaces/")
-    else:
-        form = WorkspaceForm(instance=workspace)
-    context = {
-        "form": form,
-        "organization": organization,
-    }
-    return render(request, "workspaces/workspace_edit_form.html", context)
+
+        if request.method == "POST":
+            form = WorkspaceForm(request.POST, instance=workspace)
+            try:
+                if form.is_valid():
+                    update_workspace_from_form(form=form, workspace=workspace)
+                    messages.success(request, "Workspace updated successfully.")
+                    return HttpResponseClientRedirect(f"/{organization_id}/workspaces/")
+                else:
+                    messages.error(request, "Invalid form data.")
+            except WorkspaceUpdateError as e:
+                messages.error(request, f"An error occurred: {str(e)}")
+        else:
+            form = WorkspaceForm(instance=workspace)
+            
+        context = {
+            "form": form,
+            "organization": organization,
+        }
+        return render(request, "workspaces/workspace_edit_form.html", context)
+    except Exception as e:
+        messages.error(request, f"An unexpected error occurred: {str(e)}")
+        return HttpResponseClientRedirect(f"/{organization_id}/workspaces/")

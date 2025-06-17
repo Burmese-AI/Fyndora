68c85db8d7d1e3c6403c3c30de1d2b6568a94331
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2025-06-17 23:18:18 +0700

    Refactor audit log handling to use target_entity_id and target_entity_type for improved clarity and consistency in the audit log service and selectors. Update views to accommodate new parameter names.

--- apps/auditlog/services.py
+++ apps/auditlog/services.py
@@ -1,19 +1,23 @@
+from django.contrib.contenttypes.models import ContentType
 from apps.core.utils import model_update
 
 from .models import AuditTrail
 
 
 def audit_create(
-    *, user, action_type, target_entity, target_entity_type, metadata=None
+    *, user, action_type, target_entity, metadata=None
 ):
     """
     Service to create an audit log entry.
     """
+    target_entity_type = ContentType.objects.get_for_model(target_entity)
+    target_entity_id = str(target_entity.pk)
+
     audit = AuditTrail()
     data = {
         "user": user,
         "action_type": action_type,
-        "target_entity": target_entity,
+        "target_entity_id": target_entity_id,
         "target_entity_type": target_entity_type,
         "metadata": metadata,
     }

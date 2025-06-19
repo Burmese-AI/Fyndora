cb2e707506ea20d194aa9d78c87c3aa27bc24cdd
Direwen <direwensik@gmail.com>
2025-06-19 21:38:29 +0630

    Updated Stat Overview of Org Finance Page

--- apps/entries/selectors.py
+++ apps/entries/selectors.py
@@ -4,6 +4,9 @@ from apps.organizations.models import Organization, OrganizationMember
 from apps.teams.models import TeamMember
 from django.contrib.contenttypes.models import ContentType
 from .constants import EntryType, EntryStatus
+from django.db.models import Sum
+from django.utils.timezone import now
+from datetime import timedelta
 
 
 def get_org_expenses(organization: Organization):
@@ -28,3 +31,34 @@ def get_org_expenses(organization: Organization):
     )
     
     return query
+
+def get_total_org_expenses(organization: Organization):
+    queryset = get_org_expenses(organization)
+    return queryset.aggregate(total=Sum("amount"))["total"] or 0
+
+def get_this_month_org_expenses(organization: Organization):
+    today = now().date()
+    start_of_month = today.replace(day=1)
+    queryset = get_org_expenses(organization).filter(created_at__gte=start_of_month)
+    return queryset.aggregate(total=Sum("amount"))["total"] or 0
+
+def get_average_monthly_org_expenses(organization: Organization):
+    today = now().date()
+    one_year_ago = today - timedelta(days=365)
+
+    qs = get_org_expenses(organization).filter(created_at__date__gte=one_year_ago)
+    total = qs.aggregate(total=Sum("amount"))["total"] or 0
+
+    return total / 12
+
+def get_last_month_org_expenses(organization: Organization):
+    today = now().date()
+    start_of_this_month = today.replace(day=1)
+    end_of_last_month = start_of_this_month - timedelta(days=1)
+    start_of_last_month = end_of_last_month.replace(day=1)
+
+    queryset = get_org_expenses(organization).filter(
+        created_at__date__gte=start_of_last_month,
+        created_at__date__lte=end_of_last_month
+    )
+    return queryset.aggregate(total=Sum("amount"))["total"] or 0

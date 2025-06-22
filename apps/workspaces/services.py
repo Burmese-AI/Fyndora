bd0a491a94ad9578dec2ae91c695727d04a4bf74
TharHtet236401 <tharhteta3@gmail.com>
2025-06-22 12:29:25 +0700

    Add workspace permission handling and error responses
    * Implemented permission checks for editing and deleting workspaces, providing user feedback when permissions are lacking.
    * Created a group for workspace admins and assigned permissions upon workspace creation.
    * Added functionality to delete the associated permissions group when a workspace is deleted.
    * Enhanced error handling in the workspace views to improve user experience.

--- apps/workspaces/services.py
+++ apps/workspaces/services.py
@@ -3,6 +3,9 @@ from apps.workspaces.models import Workspace
 from apps.workspaces.exceptions import WorkspaceCreationError, WorkspaceUpdateError
 from apps.core.utils import model_update
 from apps.workspaces.models import WorkspaceTeam
+from guardian.shortcuts import assign_perm
+from apps.accounts.models import CustomUser
+from django.contrib.auth.models import Group
 
 
 @transaction.atomic
@@ -22,6 +25,19 @@ def create_workspace_from_form(*, form, orgMember, organization) -> Workspace:
         workspace.organization = organization
         workspace.created_by = orgMember
         workspace.save()
+
+        group_name = f"Workspace Admins - {workspace.title} - {workspace.organization.title}"
+        group,_ = Group.objects.get_or_create(name=group_name)
+
+        assign_perm("change_workspace", group, workspace)
+        assign_perm("delete_workspace", group, workspace)
+        
+        
+        if workspace.workspace_admin is not None:
+            group.user_set.add(workspace.workspace_admin.user)
+            
+        #give permission to the organization owner
+        group.user_set.add(workspace.organization.owner.user)
         return workspace
     except Exception as e:
         raise WorkspaceCreationError(f"Failed to create workspace: {str(e)}")

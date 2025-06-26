ca7f8f9863198bdd5e9125fbd67f8364802c5c13
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2025-06-26 15:23:16 +0700

    Add new audit action type for team member addition

--- apps/auditlog/constants.py
+++ apps/auditlog/constants.py
@@ -1,4 +1,5 @@
 AUDIT_ACTION_TYPE_CHOICES = (
+    ("team_member_added", "Team Member Added"),
     ("entry_created", "Entry Created"),
     ("entry_updated", "Entry Updated"),
     ("status_changed", "Status Changed"),

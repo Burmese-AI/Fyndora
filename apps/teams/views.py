6a216dc21cc1cd6c1d13f33c2bf777946c692690
TharHtet236401 <tharhteta3@gmail.com>
2025-06-27 17:15:32 +0700

    Enhance create_team_view to include dynamic team display and response handling for HTMX requests. Added rendering of team and message HTML upon successful team creation, improving user feedback.

--- apps/teams/views.py
+++ apps/teams/views.py
@@ -7,6 +7,9 @@ from apps.workspaces.models import WorkspaceTeam
 from apps.teams.forms import TeamForm
 from apps.teams.selectors import get_teams_by_organization_id
 from apps.teams.services import create_team_from_form
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+
 
 # Create your views here.
 def teams_view(request, organization_id):
@@ -34,7 +37,23 @@ def create_team_view(request, organization_id):
             form = TeamForm(request.POST, organization=organization)
             if form.is_valid():
                 create_team_from_form(form, organization=organization)
-                messages.success(request, "Team created successfully")
+                messages.success(request, "Team created successfully.")
+                if request.headers.get("HX-Request"):
+                    teams = get_teams_by_organization_id(organization_id)
+                    attached_workspaces = []  # Initialize the variable
+                    for team in teams:
+                        attached_workspaces = WorkspaceTeam.objects.filter(team_id=team.team_id)
+                        team.attached_workspaces = attached_workspaces
+                    context = {
+                        "teams": teams,
+                        "organization": organization,
+                        "is_oob": True,
+                    }
+                    team_display_html = render_to_string("teams/partials/teams_display.html", context=context, request=request)
+                    message_html = render_to_string("includes/message.html", context=context, request=request)
+                    response = HttpResponse(f"{message_html} {team_display_html}")
+                    response["HX-trigger"] = "success"
+                    return response
                 return HttpResponseClientRedirect(f"/{organization_id}/teams/")
             else:
                 messages.error(request, "Invalid form submission")

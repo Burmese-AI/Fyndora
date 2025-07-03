b2005a32977acf60236d2a65798ad82fafc61c24
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2025-07-03 18:11:58 +0700

    Enhance email sending functionality by overriding send_mail method to support HTML and text templates, with fallback handling for missing HTML templates.

--- apps/emails/adapters.py
+++ apps/emails/adapters.py
@@ -1,10 +1,30 @@
 from allauth.account.adapter import DefaultAccountAdapter
+from django.template import TemplateDoesNotExist
+from django.template.loader import render_to_string
 
 from .services import send_email
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
     def send_mail(self, template_prefix, email, context):
-        subject = self.render_mail(f"{template_prefix}_subject", email, context).strip()
-        body = self.render_mail(f"{template_prefix}_message", email, context)
-        send_email(to=email, subject=subject, contents=body)
+        """
+        Overrides the default send_mail to use our custom email service
+        and correctly render HTML and text templates.
+        """
+        subject = render_to_string(f'{template_prefix}_subject.txt', context)
+        # Email subject *must not* contain newlines
+        subject = "".join(subject.splitlines())
+
+        # Render text and HTML versions
+        text_body = render_to_string(f'{template_prefix}_message.txt', context)
+
+        try:
+            html_body = render_to_string(f'{template_prefix}_message.html', context)
+        except TemplateDoesNotExist:
+            html_body = None
+
+        # Our email service will send the HTML version if it exists,
+        # otherwise it will send the plain text version.
+        contents = html_body if html_body else text_body
+
+        send_email(to=email, subject=subject, contents=contents)

feccb40b983145dfa4c3431ac67a6688ab90a06d
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2025-07-08 10:41:31 +0700

    Disable email verification during development

--- config/settings.py
+++ config/settings.py
@@ -245,7 +245,7 @@ ACCOUNT_ADAPTER = "apps.emails.adapters.CustomAccountAdapter"
 ACCOUNT_LOGIN_METHODS = ('email',)
 ACCOUNT_SIGNUP_FIELDS = ('email*', 'username*', 'password1*', 'password2*')
 ACCOUNT_UNIQUE_EMAIL = True
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION = "none"
 ACCOUNT_SESSION_REMEMBER = True
 
 # Using dummy backend because we are using our own email service

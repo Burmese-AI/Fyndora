9c32a4938683bf0cb4a4863a458f13c5cb6f3368
Thar Htet <149906483+TharHtet236401@users.noreply.github.com>
2025-07-31 11:56:30 +0700

    Refactor permission utility imports and clean up whitespace in organization and workspace expense views. Ensure consistent formatting for permission checks in context data. (#134)

--- apps/entries/utils.py
+++ apps/entries/utils.py
@@ -1,5 +1,6 @@
 from apps.core.permissions import OrganizationPermissions, WorkspacePermissions
 
+
 def can_view_org_expense(user, organization):
     """
     Returns True if the user has the permission to view the organization expense.
@@ -13,11 +14,13 @@ def can_add_org_expense(user, organization):
     """
     return user.has_perm(OrganizationPermissions.ADD_ORG_ENTRY, organization)
 
+
 def can_update_org_expense(user, organization):
     """
     Returns True if the user has the permission to update the organization expense.
     """
     return user.has_perm(OrganizationPermissions.CHANGE_ORG_ENTRY, organization)
+
 
 def can_delete_org_expense(user, organization):
     """
@@ -25,17 +28,20 @@ def can_delete_org_expense(user, organization):
     """
     return user.has_perm(OrganizationPermissions.DELETE_ORG_ENTRY, organization)
 
+
 def can_add_workspace_expense(user, workspace):
     """
     Returns True if the user has the permission to add the workspace expense.
     """
     return user.has_perm(WorkspacePermissions.ADD_WORKSPACE_ENTRY, workspace)
 
+
 def can_update_workspace_expense(user, workspace):
     """
     Returns True if the user has the permission to update the workspace expense.
     """
     return user.has_perm(WorkspacePermissions.CHANGE_WORKSPACE_ENTRY, workspace)
+
 
 def can_delete_workspace_expense(user, workspace):
     """

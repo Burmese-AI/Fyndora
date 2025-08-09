0b2694cad801acaa71396af60d973b552fae5acc
TharHtet236401 <tharhteta3@gmail.com>
2025-08-09 17:28:10 +0700

    Refactor remittance list view to include workspace filtering and enhance error handling. Update template to add workspace selection dropdown for better user experience.

--- apps/remittance/views.py
+++ apps/remittance/views.py
@@ -15,92 +15,8 @@ from .models import Remittance
 from .selectors import get_remittances_with_filters
 from .services import remittance_confirm_payment
 from apps.organizations.models import Organization
-from apps.core.selectors import get_organization_by_id, get_remiitances_under_organization
+from apps.core.selectors import get_organization_by_id, get_remiitances_under_organization, get_workspaces_under_organization
 from django.core.paginator import Paginator
-
-#this view will not be currently used 
-class RemittanceListView(LoginRequiredMixin, ListView):
-    """
-    Main template-based view for listing remittances with filters.
-    """
-
-    model = Remittance
-    template_name = "remittance/index.html"
-    context_object_name = "remittances"
-    paginate_by = PAGINATION_SIZE
-
-    def get_template_names(self):
-        # If the request is from htmx, return the partial template
-        if self.request.META.get("HTTP_HX_REQUEST"):
-            return ["remittance/remittance_list.html"]
-        # Otherwise, return the full template
-        return [self.template_name]
-
-    def get_queryset(self):
-        if "workspace_id" not in self.kwargs:
-            raise Http404("Workspace ID not found in URL parameters.")
-
-        return get_remittances_with_filters(
-            workspace_id=self.kwargs.get("workspace_id"),
-            team_id=self.request.GET.get("team"),
-            status=self.request.GET.get("status"),
-            start_date=self.request.GET.get("start_date"),
-            end_date=self.request.GET.get("end_date"),
-            search=self.request.GET.get("q"),
-        )
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        workspace_id = self.kwargs.get("workspace_id")
-
-        # Get current workspace and add it to context
-        workspace = Workspace.objects.filter(pk=workspace_id).first()
-        context["workspace"] = workspace
-
-        # Get all teams and statuses for filter dropdowns
-        if workspace:
-            context["all_teams"] = Team.objects.filter(
-                workspace_teams__workspace=workspace
-            ).order_by("title")
-        else:
-            context["all_teams"] = Team.objects.none()
-
-        context["all_statuses"] = RemittanceStatus.choices
-
-        # Preserve existing filters for pagination and search query
-        context["current_filters"] = self.request.GET.copy()
-        if "page" in context["current_filters"]:
-            context["current_filters"].pop("page")
-        context["search_query"] = self.request.GET.get("q", "")
-        context["workspace_id"] = self.kwargs.get("workspace_id")
-        return context
-
-#this view will not be currently used 
-class RemittanceConfirmPaymentView(LoginRequiredMixin, View):
-    """
-    View to handle the confirmation of a remittance payment.
-    """
-
-    def post(self, request, *args, **kwargs):
-        remittance_id = kwargs.get("remittance_id")
-        remittance = get_object_or_404(Remittance, pk=remittance_id)
-
-        try:
-            updated_remittance = remittance_confirm_payment(
-                remittance=remittance, user=request.user
-            )
-
-            context = {
-                "remittance": updated_remittance,
-                "workspace": updated_remittance.workspace,
-            }
-            return render(request, "remittance/remittance_row.html", context)
-
-        except (PermissionDenied, ValidationError) as e:
-            messages.error(request, str(e))
-            response = HttpResponse(status=204)
-            response["HX-Refresh"] = "true"
-            return response
 
 
 #developed by THA for the remittance list by each workspace team
@@ -111,6 +27,7 @@ def remittance_list_view(request, organization_id):
     """
     try:
         organization = get_organization_by_id(organization_id)
+        workspaces = get_workspaces_under_organization(organization_id)
         remittances = get_remiitances_under_organization(organization_id)
         paginator = Paginator(remittances, PAGINATION_SIZE)
         page_number = request.GET.get('page')
@@ -121,6 +38,8 @@ def remittance_list_view(request, organization_id):
             "is_paginated": page_obj.has_other_pages(),
             "page_obj": page_obj,
             "paginator": paginator,
+            "workspaces": workspaces, # for dropdown filter
+
         }
         return render(request, "remittance/index.html", context)
     except Exception as e:
@@ -128,3 +47,89 @@ def remittance_list_view(request, organization_id):
         print(f"Error in remittance_list_view: {e}")
         return render(request, "remittance/index.html")
     
+
+#this view will not be currently used 
+# class RemittanceListView(LoginRequiredMixin, ListView):
+#     """
+#     Main template-based view for listing remittances with filters.
+#     """
+
+#     model = Remittance
+#     template_name = "remittance/index.html"
+#     context_object_name = "remittances"
+#     paginate_by = PAGINATION_SIZE
+
+#     def get_template_names(self):
+#         # If the request is from htmx, return the partial template
+#         if self.request.META.get("HTTP_HX_REQUEST"):
+#             return ["remittance/remittance_list.html"]
+#         # Otherwise, return the full template
+#         return [self.template_name]
+
+#     def get_queryset(self):
+#         if "workspace_id" not in self.kwargs:
+#             raise Http404("Workspace ID not found in URL parameters.")
+
+#         return get_remittances_with_filters(
+#             workspace_id=self.kwargs.get("workspace_id"),
+#             team_id=self.request.GET.get("team"),
+#             status=self.request.GET.get("status"),
+#             start_date=self.request.GET.get("start_date"),
+#             end_date=self.request.GET.get("end_date"),
+#             search=self.request.GET.get("q"),
+#         )
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         workspace_id = self.kwargs.get("workspace_id")
+
+#         # Get current workspace and add it to context
+#         workspace = Workspace.objects.filter(pk=workspace_id).first()
+#         context["workspace"] = workspace
+
+#         # Get all teams and statuses for filter dropdowns
+#         if workspace:
+#             context["all_teams"] = Team.objects.filter(
+#                 workspace_teams__workspace=workspace
+#             ).order_by("title")
+#         else:
+#             context["all_teams"] = Team.objects.none()
+
+#         context["all_statuses"] = RemittanceStatus.choices
+
+#         # Preserve existing filters for pagination and search query
+#         context["current_filters"] = self.request.GET.copy()
+#         if "page" in context["current_filters"]:
+#             context["current_filters"].pop("page")
+#         context["search_query"] = self.request.GET.get("q", "")
+#         context["workspace_id"] = self.kwargs.get("workspace_id")
+#         return context
+
+# #this view will not be currently used 
+# class RemittanceConfirmPaymentView(LoginRequiredMixin, View):
+#     """
+#     View to handle the confirmation of a remittance payment.
+#     """
+
+#     def post(self, request, *args, **kwargs):
+#         remittance_id = kwargs.get("remittance_id")
+#         remittance = get_object_or_404(Remittance, pk=remittance_id)
+
+#         try:
+#             updated_remittance = remittance_confirm_payment(
+#                 remittance=remittance, user=request.user
+#             )
+
+#             context = {
+#                 "remittance": updated_remittance,
+#                 "workspace": updated_remittance.workspace,
+#             }
+#             return render(request, "remittance/remittance_row.html", context)
+
+#         except (PermissionDenied, ValidationError) as e:
+#             messages.error(request, str(e))
+#             response = HttpResponse(status=204)
+#             response["HX-Refresh"] = "true"
+#             return response
+
+
